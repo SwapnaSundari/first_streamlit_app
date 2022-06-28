ea939ad35ed97c821bffabab18f1d7c9a65906a0
SwapnaSundari <108303498+SwapnaSundari@users.noreply.github.com>
2022-06-28 08:50:59 +0200

    Update Streamlitapp.py
    Added new header

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -4,7 +4,10 @@ streamlit.title('🥗 My Mom's New Healthy Diner')
 
 streamlit.header('🐔 BreakFast Menu')
 
-streamlit.text('🥣 Omega 3 and BlueBerry Oatmeal')
+streamlit.text('🥣 Omega 3 and BlueBerry Oatmeal')  
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text(' 🐔 Hard Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
+
+                
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
